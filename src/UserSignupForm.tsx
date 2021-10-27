83ebc482748cce80da19c1f40c8d011ec42fee2e
Colin Sidberry <colinsidberry@gmail.com>
2021-10-26 17:32:53 -0700

    adjusting signup form.

--- src/UserSignupForm.tsx
+++ src/UserSignupForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { UserSignupInterface } from "./interfaces";
+import { Button, Card, CardBody, CardTitle } from "reactstrap";
 /** Form for signing up a user.
  *
  * props:
@@ -46,73 +47,81 @@ function UserSignupForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-                id="username"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-            />
-            <label htmlFor="firstName">First:</label>
-            <input
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-            />
+        // <Card>
+        //     <CardBody>
+                <form onSubmit={handleSubmit}>
+                    {/* <CardTitle> */}
+                        <label htmlFor="username">Username:</label>
+                    {/* </CardTitle> */}
+                    <input
+                        id="username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="firstName">First:</label>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                    />
 
-            <label htmlFor="lastName">Last:</label>
-            <input
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-            />
-            <label htmlFor="email">Email:</label>
-            <input
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-            />
-            <label htmlFor="hobbies">Hobbies:</label>
-            <textarea
-                id="hobbies"
-                name="hobbies"
-                value={formData.hobbies}
-                onChange={handleChange}
-            />
-            <label htmlFor="interests">Interests:</label>
-            <textarea
-                id="interests"
-                name="interests"
-                value={formData.interests}
-                onChange={handleChange}
-            />
-            <label htmlFor="zipCode">Zip Code:</label>
-            <input
-                id="zipCode"
-                name="zipCode"
-                value={formData.zipCode}
-                onChange={handleChange}
-            />
-            <label htmlFor="image">Image:</label>
-            <input
-                id="image"
-                name="image"
-                value={formData.image}
-                onChange={handleChange}
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-            />
-            <button>Sign Up!</button>
-        </form>
+                    <label htmlFor="lastName">Last:</label>
+                    <input
+                        id="lastName"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="hobbies">Hobbies:</label>
+                    <textarea
+                        id="hobbies"
+                        name="hobbies"
+                        value={formData.hobbies}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="interests">Interests:</label>
+                    <textarea
+                        id="interests"
+                        name="interests"
+                        value={formData.interests}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="zipCode">Zip Code:</label>
+                    <input
+                        id="zipCode"
+                        name="zipCode"
+                        value={formData.zipCode}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="image">Image:</label>
+                    <input
+                        id="image"
+                        name="image"
+                        type="file"
+                        value={formData.image}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    <button>Sign Up!</button>
+                </form>
+        //     </CardBody>
+        // </Card>
     );
 }
 
